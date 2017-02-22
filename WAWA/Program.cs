8e3fe24199734ddbde98c7e3271421c6378bf9a0
Yendiyarov <s.endiyarov@gmail.com>
2017-02-23 00:02:09 +0500

    Initial commit

---
+++ WAWA/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WAWA
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("OLESHA");
+            Console.ReadLine();
+        }
+    }
+}
